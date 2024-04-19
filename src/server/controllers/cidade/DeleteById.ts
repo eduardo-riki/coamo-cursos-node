26862f84d21a88931b48006664870577de619db1
= <=>
2024-04-19 00:07:51 -0300

    feat: Criar método de DeleteById para cidade

---
+++ src/server/controllers/cidade/DeleteById.ts
@@ -0,0 +1,22 @@
+import { Request, RequestHandler, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import * as Yup from "yup";
+
+import { validation } from "../../shared/middleware";
+
+interface IParamProps {
+  id?: number;
+}
+
+export const deleteByIdValidation = validation((getSchema) => ({
+  params: getSchema<IParamProps>(
+    Yup.object().shape({
+      id: Yup.number().integer().required().moreThan(0),
+    })
+  ),
+}));
+
+export const deleteById = async (req: Request<IParamProps>, res: Response) => {
+  console.log(req.params);
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Não implementado!");
+};

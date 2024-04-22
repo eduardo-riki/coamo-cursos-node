6fcd8c9a9258ffab175d3e32a1d734b2315aff6c
Eduardo Riki <eduardomatushita1@gmail.com>
2024-04-22 09:45:37 -0300

    feat: Criar migration para criar tabela de CIdade

---
+++ src/server/database/migrations/0000_create_cidade.ts
@@ -0,0 +1,18 @@
+import type { Knex } from "knex";
+import { ETableNames } from "../ETableNames";
+
+export async function up(knex: Knex) {
+  knex.schema
+    .createTable(ETableNames.cidade, (table) => {
+      table.bigIncrements("id").primary().index(),
+        table.string("nome", 100).index().notNullable(),
+        table.comment("Tabela usada para armazenar Cidades do sistema");
+    })
+    .then(() => {
+      console.log(`# Create table ${ETableNames.cidade}`);
+    });
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable(ETableNames.cidade);
+}

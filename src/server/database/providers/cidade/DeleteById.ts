457652bcdcaa7c63e958c5e0d37345ffadb9ca6d
Eduardo Riki <eduardomatushita1@gmail.com>
2024-05-07 11:26:13 -0300

    feat: Finalizado providers de Cidade para CRUD

--- src/server/database/providers/cidade/DeleteById.ts
+++ src/server/database/providers/cidade/DeleteById.ts
@@ -4,10 +4,9 @@ import { ETableNames } from "../../ETableNames";
 
 export const deleteById = async (id: number): Promise<void | Error> => {
   try {
-    const result = await Knex(ETableNames.cidade).where("id", "=", id).del();
+    const result = await Knex(ETableNames.cidade).where("id", id).del();
 
     if (result > 0) return;
-
     return new Error("Erro ao remover o registro.");
   } catch (error) {
     return new Error("Erro ao remover o registro.");

890e653e0bd433e401171bcbfcab9396de8b95b2
Eduardo Riki <eduardomatushita1@gmail.com>
2024-05-09 10:17:37 -0300

    test: Corrigir arquivos de testes para :memory: do SQLite

--- tests/cidade/GetAll.test.ts
+++ tests/cidade/GetAll.test.ts
@@ -3,11 +3,25 @@ import { testServer } from "../jest.setup";
 
 describe("Cidade - Get All", () => {
   it("Listar todos os registros", async () => {
+    await testServer.post("/cidade").send({
+      nome: "Goioerê",
+      estado: "Paraná",
+    });
+
+    await testServer.post("/cidade").send({
+      nome: "Campo Mourão",
+      estado: "Paraná",
+    });
     const res1 = await testServer.get("/cidade/listar");
 
-    expect(Number(res1.header["x-total-count"])).toBeGreaterThan(0);
     expect(res1.statusCode).toEqual(StatusCodes.OK);
     expect(res1.body.length).toBeGreaterThan(0);
+
+    const res2 = await testServer.get("/cidade/listar?filter=Goio");
+
+    expect(Number(res2.header["x-total-count"])).toBeGreaterThan(0);
+    expect(res2.statusCode).toEqual(StatusCodes.OK);
+    expect(res2.body.length).toBeGreaterThan(0);
     // expect(typeof res1.body).toEqual("number");
   });
 });

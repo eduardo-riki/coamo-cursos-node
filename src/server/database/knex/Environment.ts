ac33de80713112ff2b7dd1d6ed6c1f2896a6f071
Eduardo Riki <eduardomatushita1@gmail.com>
2024-05-09 10:17:59 -0300

    feat: Adicionar conexão com PostgreSQL

--- src/server/database/knex/Environment.ts
+++ src/server/database/knex/Environment.ts
@@ -1,43 +1,53 @@
 import { Knex } from "knex";
 import path from "path";
+import "dotenv/config";
 
 export const development: Knex.Config = {
-  client: "sqlite3",
+  client: "pg",
   useNullAsDefault: true,
   connection: {
-    filename: path.resolve(__dirname, "..", "..", "..", "..", "database.sqlite")
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    ssl: { rejectUnauthorized: false },
   },
   migrations: {
-    directory: path.resolve(__dirname, "..", "migrations")
+    directory: path.resolve(__dirname, "..", "migrations"),
   },
   seeds: {
-    directory: path.resolve(__dirname, "..", "seeds")
+    directory: path.resolve(__dirname, "..", "seeds"),
   },
   pool: {
     afterCreate: (connection: any, done: Function) => {
-      connection.run("PRAGMA foreign_keys = ON");
+      connection.query("SET CONSTRAINTS ALL IMMEDIATE;", (error: any) => {
+        if (error) {
+          console.error("Error enabling foreign key constraints:", error);
+        }
+      });
       done();
-    }
-  }
+    },
+  },
 };
 
 export const test: Knex.Config = {
-  ...development,
+  client: "sqlite3",
+  useNullAsDefault: true,
   connection: {
     filename: ":memory:",
   },
   migrations: {
-    directory: path.resolve(__dirname, "..", "migrations")
+    directory: path.resolve(__dirname, "..", "migrations"),
   },
   seeds: {
-    directory: path.resolve(__dirname, "..", "seeds")
+    directory: path.resolve(__dirname, "..", "seeds"),
   },
   pool: {
     afterCreate: (connection: any, done: Function) => {
       connection.run("PRAGMA foreign_keys = ON");
       done();
-    }
-  }
+    },
+  },
 };
 
 export const production: Knex.Config = {

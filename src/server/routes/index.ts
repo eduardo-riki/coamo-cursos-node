2372b87c308e7dfe92107194204ed26d1e8a5bd4
Eduardo Riki <eduardomatushita1@gmail.com>
2024-05-20 11:22:35 -0300

    feat: Adicionar autenticação nas rotas

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -4,35 +4,37 @@ import { StatusCodes } from "http-status-codes";
 import { CidadeController } from "../controllers";
 import { PessoaController } from "../controllers";
 import { UsuarioController } from "../controllers/usuario";
+import { ensureAuthenticated } from "../shared/middleware/ensureAuthenticated";
 
 const router = Router();
 
 router.post(
   "/cidade",
+  ensureAuthenticated,
   CidadeController.createValidation,
   CidadeController.create
 );
-
 router.get(
   "/cidade/listar",
+  ensureAuthenticated,
   CidadeController.getAllValidation,
   CidadeController.getAll
 );
-
 router.get(
   "/cidade/:id",
+  ensureAuthenticated,
   CidadeController.getByIdValidation,
   CidadeController.getById
 );
-
 router.put(
   "/cidade/:id",
+  ensureAuthenticated,
   CidadeController.updateByIdValidation,
   CidadeController.updateById
 );
-
 router.delete(
   "/cidade/:id",
+  ensureAuthenticated,
   CidadeController.deleteByIdValidation,
   CidadeController.deleteById
 );
@@ -41,30 +43,31 @@ router.delete(
 
 router.post(
   "/pessoa",
+  ensureAuthenticated,
   PessoaController.createValidation,
   PessoaController.create
 );
-
 router.get(
   "/pessoa/listar",
+  ensureAuthenticated,
   PessoaController.getAllValidation,
   PessoaController.getAll
 );
-
 router.get(
   "/pessoa/:id",
+  ensureAuthenticated,
   PessoaController.getByIdValidation,
   PessoaController.getById
 );
-
 router.put(
   "/pessoa/:id",
+  ensureAuthenticated,
   PessoaController.updateByIdValidation,
   PessoaController.updateById
 );
-
 router.delete(
   "/pessoa/:id",
+  ensureAuthenticated,
   PessoaController.deleteByIdValidation,
   PessoaController.deleteById
 );
@@ -76,7 +79,6 @@ router.post(
   UsuarioController.signUpValidation,
   UsuarioController.signUp
 );
-
 router.post(
   "/usuario/signIn",
   UsuarioController.signInValidation,

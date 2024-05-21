625986ccb3bf6b8570dd2bf625780d2d22d85cb9
Eduardo Riki <eduardomatushita1@gmail.com>
2024-05-21 10:49:15 -0300

    feat: Adicionar CORS

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,10 +1,16 @@
-import express from 'express';
+import express from "express";
 
-import './shared/services/TranslationError';
-import { router } from './routes';
+import "./shared/services/TranslationError";
+import { router } from "./routes";
+import cors from "cors";
 
 const server = express();
 
+server.use(
+  cors({
+    origin: process.env.ENABLED_CORS?.split(";") || [],
+  })
+);
 server.use(express.json());
 server.use(router);
 
